cb2abf0d642034bfaf38d829f3ef9a478c0ccacc
ToporBogdan <bogdantopor98@gmail.com>
2022-04-13 13:38:04 +0300

    Added more functions for linked lists

--- proiect_lista.c
+++ proiect_lista.c
@@ -43,34 +43,126 @@ ListNode *getNewItem(const int value)
     return NewItem; 
 }
 
-ListNode addNodeInList(ListNode **head, const int number, const int poz)
+ListNode* addNodeAtBeg(ListNode* head, const int value)
 {
-    ListNode *traverse;
-    traverse = *head;
-    int NodePos = 0;
-    ListNode *addedNumber;
+    ListNode* addedNumber;
+    addedNumber = getNewItem(value);
+    addedNumber->nextItem = head;
+    head = addedNumber;
+    return head;
+}
+
+ListNode* addNodeInList(ListNode* head, const int number, const int poz)
+{
+    ListNode* traverse;
+    traverse = head;
+    int NodePos = 1;
+    ListNode* addedNumber;
     addedNumber = getNewItem(number);
 
-    if (NodePos == 0)
+    while (traverse == NULL)
     {
-        addedNumber->nextItem = *head;
-        *head = addedNumber;
+        head = addedNumber;
+    }
+
+    if (poz == 1)
+    {
+        head = addNodeAtBeg(head, number);
+        traverse = head;
     }
 
     while (traverse != NULL)
     {
-        if (NodePos == poz-2)
+        if (NodePos == poz-1)
         {
             addedNumber->nextItem = traverse->nextItem;
             traverse->nextItem = addedNumber;
         }
-        
+        // printf("Traverse are valoarea %d \n", traverse->value);
         traverse = traverse->nextItem;
-        printf("Suntem la pozitia %d \n", NodePos);
+        // printf("Suntem la pozitia %d \n", NodePos);
         NodePos = NodePos + 1;
         
     }
-    return;
+    return head; 
+    
+}
+
+ListNode* removeNodeFromList(ListNode* head, const int poz)
+{
+    ListNode *current, *prev;
+    current = head;
+    int Nodepos = 1;
+
+    if (head == NULL)
+    {
+        printf("Nothing to remove. The list is already empty.");
+    }
+    else if (poz == 1)
+    {
+        head = current->nextItem;
+        free(current);
+        current = NULL;
+    }
+    else
+    {
+        while (poz != 1)
+        {
+            prev = current;
+            current = current->nextItem;
+            Nodepos = Nodepos + 1;
+            if (Nodepos == poz)
+            {
+                prev->nextItem = current->nextItem;
+                free(current);
+                current = NULL;
+                break;
+            }
+            
+        }
+        
+    }
+    return head;
+}
+
+void isValueInList(ListNode* head, const int number)
+{
+    ListNode* traverse;
+    traverse = head;
+    int NodePos = 1;
+    int check = 0;
+    while (traverse != NULL)
+    {
+        if (traverse->value == number)
+        {
+            printf("The value %d is present in the list, on the %d position \n", number, NodePos);
+            check = 1;
+        }
+        traverse = traverse->nextItem;
+        NodePos = NodePos + 1;
+
+    }
+    if (check == 0)
+    {
+        printf("The number %d is not in this list \n", number);
+    }
+    
+}
+
+void freeList(ListNode* head)
+{
+    ListNode* traverse;
+    traverse = head;
+    while (traverse != NULL)
+    {
+        traverse = traverse->nextItem;
+        free(head);
+        head = traverse;
+    }
+    if (head == NULL)
+    {
+        printf("The list has been removed and the memory was dinamically freed \n");
+    }
     
 }
 
@@ -94,14 +186,18 @@ int main(int argc, char const *argv[])
 
 
     printList(head);
-    addNodeInList(&head, 99, 4);
-    printList(head);
 
+    ListNode* testADD;
+    testADD = addNodeInList(head, 99, 6);
+    printList(testADD);
 
+    ListNode* testRemove;
+    testRemove = removeNodeFromList(head, 4);
+    printList(testRemove);
 
+    isValueInList(head, 333);
 
-
-
+    freeList(head);
 
 
 
